504060a9e4d62abfd76723836f118215134223b6
sabbir <sabbirrahman42@iut-dhaka.edu>
2021-10-02 23:54:56 +0600

    user emphatetical challenge model created

---
+++ backend/model/userChallengeModel.js
@@ -0,0 +1,30 @@
+const mongoose  =  require('mongoose')
+
+//this is for replicating required
+const requiredString = {
+    type: String,
+    required: true
+}
+
+const requiredNumber = {
+    type: Number,
+    required: true
+}
+
+const requiredUniqueString = {
+    type: String,
+    required: true,
+    unique: true
+}
+
+const userChallengeSchema = mongoose.Schema({
+    user_email: requiredString,
+    challenge_id: requiredString,
+    complete_time: Number,
+    timestamp: {
+        type: Date,
+        default: new Date(),
+    }
+})
+
+module.exports = mongoose.model('userChallenge', userChallengeSchema)
